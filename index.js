852a7423d594f890cb4e47aad50b63b8fb61d9fe
cristian287 <cristianalvarado287@gmail.com>
2024-01-23 00:20:56 -0300

    feat:se creo el server de express

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 const { google } = require("googleapis");
 const { Client, LocalAuth } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
-
+require ("dotenv").config()
 const credentialsPath = "keys.json";
 process.env.GOOGLE_APPLICATION_CREDENTIALS = credentialsPath;
+
+// Importar la librería express
+const express = require('express');
+
+// Crear una instancia de la aplicación express
+const app = express();
+
+// Definir el puerto donde escuchará el servidor
+const port = 3080;
+
+// Usar el método express.static para servir los archivos estáticos de la carpeta "public"
+app.use(express.static('public'));
+
+// Definir una función que maneje las peticiones a la ruta raíz
+app.get('/', (req, res) => {
+  // Enviar una respuesta con el texto "Hola mundo"
+  res.sendFile('public/index.html');
+});
+
+
+
+// Iniciar el servidor en el puerto especificado
+app.listen(port, () => {
+  // Mostrar un mensaje en la consola indicando que el servidor está listo
+  console.log(`Servidor listo en http://localhost:${port}`);
+});
+
+// comentario de separacion
 
 const client = new Client({
   puppeteer: {
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   },
-  authStrategy: new LocalAuth(),
+  authStrategy: new LocalAuth()
 });
 
 const SPREADSHEET_ID = process.env.SPREADSHEET_ID;
@@ -81,3 +109,4 @@ client.on("ready", () => {
 });
 
 client.initialize();
+
