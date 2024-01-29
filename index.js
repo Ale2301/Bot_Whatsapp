e57b1c5101abfe7d044afadf4e8e2dcd5a99ca63
Ale2301 <alejo.2001@live.com.ar>
2024-01-29 12:38:40 -0300

    fix: estetica de codigo

--- index.js
+++ index.js
@@ -1,43 +1,20 @@
 const { google } = require("googleapis");
 const { Client, LocalAuth } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
-require ("dotenv").config()
+require("dotenv").config();
 const credentialsPath = "keys.json";
 process.env.GOOGLE_APPLICATION_CREDENTIALS = credentialsPath;
 
-// Importar la librería express
-const express = require('express');
-
-// Crear una instancia de la aplicación express
+//Express server
+const express = require("express");
 const app = express();
-
-// Definir el puerto donde escuchará el servidor
 const port = 3080;
-
-// Usar el método express.static para servir los archivos estáticos de la carpeta "public"
-app.use(express.static('public'));
-
-// Definir una función que maneje las peticiones a la ruta raíz
-app.get('/', (req, res) => {
-  // Enviar una respuesta con el texto "Hola mundo"
-  res.sendFile('public/index.html');
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile("public/index.html");
 });
-
-
-
-// Iniciar el servidor en el puerto especificado
 app.listen(port, () => {
-  // Mostrar un mensaje en la consola indicando que el servidor está listo
   console.log(`Servidor listo en http://localhost:${port}`);
-});
-
-// comentario de separacion
-
-const client = new Client({
-  puppeteer: {
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  },
-  authStrategy: new LocalAuth()
 });
 
 const SPREADSHEET_ID = process.env.SPREADSHEET_ID;
@@ -46,6 +23,13 @@ if (!SPREADSHEET_ID) {
   console.error("No existe la variable SPREADSHEET_ID");
   throw new Error("No existe la variable SPREADSHEET_ID");
 }
+
+const client = new Client({
+  puppeteer: {
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  },
+  authStrategy: new LocalAuth(),
+});
 
 async function goToSheet() {
   try {
@@ -109,4 +93,3 @@ client.on("ready", () => {
 });
 
 client.initialize();
-
